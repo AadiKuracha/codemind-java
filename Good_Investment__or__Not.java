e76829cf4e25d4e427f4778a6589b03ed485ab10
AadiKuracha <119741288+AadiKuracha@users.noreply.github.com>
2024-01-24 12:29:11 +0530

    import java.util.*; public class Myname{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         if(2*b>a) System.out.println("NO");         else System.out.println("YES");     } }

---
+++ Good_Investment__or__Not.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Myname{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(2*b>a) System.out.println("NO");
+        else System.out.println("YES");
+    }
+}
